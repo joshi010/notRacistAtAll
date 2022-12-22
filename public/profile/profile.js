eef00ceeae3422cffd98357e266189243090cf63
j_venegas <josuesinvr1@gmail.com>
2022-12-22 01:29:02 -0600

    add phone compatibility and usernames

--- public/profile/profile.js
+++ public/profile/profile.js
@@ -5,23 +5,61 @@ let race;
 
 
 img.src = localStorage.getItem('image');
-rc.innerHTML = '@' + localStorage.getItem('race');
+
+
+const brownUser = () => {
+    const first = ['the', 'xX', 'el', 'super']
+    const second = ['tacoEater', 'wallJump', 'brownie', 'notNig', 'dark', 'mencho'];
+    let random = Math.floor(Math.random()* first.length);
+    let random2 = Math.floor(Math.random()* second.length);
+    let randGen = Math.floor(Math.random()*720);
+
+    let rendered = `${first[random]}${second[random2]}${randGen}`;
+    return rendered
+}
+
+const whiteUser = () => {
+    const first = ['proudly', 'cool', 'mega', 'daily']
+    const second = ['racist', 'schoolShooter', 'warStarter', 'pedoPhile', 'twinkie', 'gay'];
+    let random = Math.floor(Math.random()* first.length);
+    let random2 = Math.floor(Math.random()* second.length);
+    let randGen = Math.floor(Math.random()*720);
+
+    let rendered = `${first[random]}${second[random2]}${randGen}`;
+    return rendered
+}
+
+const blackUser = () => {
+    const first = ['monkey', 'darker', 'shadow', 'notVisible'];
+    const second = ['kool-Aid', 'chickenEater', 'robber', 'NWord', 'Obama', 'watermelonLover'];
+    let random = Math.floor(Math.random()* first.length);
+    let random2 = Math.floor(Math.random()* second.length);
+    let randGen = Math.floor(Math.random()*720);
+
+    let rendered = `${first[random]}${second[random2]}${randGen}`;
+    return rendered
+}
 
 
 switch(localStorage.getItem('race')) {
     case('m'):
         race = 'brown';
+        localStorage.setItem('user', brownUser());
         break;
 
     case('b'):
         race = 'white';
+        localStorage.setItem('user', whiteUser());
         break;
     
     case('n'):
         race = 'black';
+        localStorage.setItem('user', blackUser());
         break;
     default:
         console.log('Bastard');
 }
+
+rc.innerHTML = '@' + localStorage.getItem('user');
 let phrase = 'Imagine being ' + race + ' 😹😹😹';
 desc.innerHTML = phrase;
